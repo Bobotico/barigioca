992a2ab54dfb14c7d959e1273b82a2f0d91b6339
Bobotico <francesco.borrelli.pro@gmail.com>
2022-09-20 13:13:09 +0200

    nineth

--- src/components/Views/Corsi.js
+++ src/components/Views/Corsi.js
@@ -14,15 +14,16 @@ function Corsi({ corsi }) {
               <div className="corso col-md-6 col-12">
                 <img className="imageCorso d-block mx-auto shadow" src={corso.image} alt="" />
               </div>
-              <div className="infoCorso col-md-6 col-12" /*con la parte di codice che segue cambiamo le dimensioni del font dell'header*/ style={{ fontSize: '1.5em' }}>
-                <h1 className="nomeCorso">{corso.name}</h1>
-                <p className="descrizioneCorso">{corso.description}</p>
-                {/*<div className="alignButton">
-                  <Link to="/Corso1" className="buttonCorso btn btn-lg btn-success rounded-pill">
-                    More info
-                  </Link>
-                </div>*/}
-              </div>
+
+                <div className="infoCorso col-md-6 col-12" /*con la parte di codice che segue cambiamo le dimensioni del font dell'header*/ style={{ fontSize: '1.5em' }}>
+                  <h1 className="nomeCorso">{corso.name}</h1>
+                  <p className="descrizioneCorso">{corso.description}</p>
+                  {/*<div className="alignButton">
+                    <Link to="/Corso1" className="buttonCorso btn btn-lg btn-success rounded-pill">
+                      More info
+                    </Link>
+                  </div>*/}
+                </div>
             </div>
           </div>
         </section>
